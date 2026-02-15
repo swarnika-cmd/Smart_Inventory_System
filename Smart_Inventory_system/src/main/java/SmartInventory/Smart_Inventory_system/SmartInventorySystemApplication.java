efd19308479d479ec15ee97e8e258436e8ad5a7a
swarnika-cmd <swarnika.24bcs10482@sst.scaler.com>
2026-02-16 02:57:30 +0530

    first commit

---
+++ Smart_Inventory_system/src/main/java/SmartInventory/Smart_Inventory_system/SmartInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package SmartInventory.Smart_Inventory_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInventorySystemApplication.class, args);
+	}
+
+}
---
+++ Smart_Inventory_system/src/test/java/SmartInventory/Smart_Inventory_system/SmartInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package SmartInventory.Smart_Inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
